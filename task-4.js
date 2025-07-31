ffe8a34d6d68265227ffc8794ff2f4874ef5a130
tnayem <tasdidnayem@gmail.com>
2025-07-31 13:10:38 +0600

    task-5 compleate

--- task-4.js
+++ task-4.js
@@ -4,15 +4,7 @@
 // sample-input: I am learning Programming to become a programmer
 
 // sample-output: Programming
-// const myName = "Md Tasdid Nayemaaaaaa";
-// const singleName = myName.split(" ")
-// let largestWord = "a"
-// for(const singleWOrd of singleName){
-//     if(singleWOrd.length>largestWord.length){
-//         largestWord = singleWOrd
-//     }
-// }
-// console.log(largestWord)
+
 function longestWord(sentences){
     const singleWOrds = sentences.split(" ")
     let largestWord = "a"
@@ -23,5 +15,6 @@ function longestWord(sentences){
     }
     return largestWord;
 }
-const myName = longestWord("My name Md Tasdid Nayem")
+const myName = longestWord("I am learning Programming to become a programmer")
 console.log(myName)
+
---
+++ task-5.js
@@ -0,0 +1,4 @@
+// Task-5:
+// Generate a random number between 10 to 20.
+const randomNumber = Math.ceil(Math.random() * (20 - 10)) + 10;
+console.log(randomNumber);
