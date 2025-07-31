d4ed35e653f1ef11b45f6fd7a154d9dfbc862642
tnayem <tasdidnayem@gmail.com>
2025-07-31 10:10:59 +0600

    Task 1 is Solve

--- problem-1.js
+++ problem-1.js
@@ -1,2 +1,8 @@
 // Task-1:
 // Write a function to convert temperature from Celsius to Fahrenheit.
+function cToF(c){
+    const f = (c*9/5)+32;
+    return f;
+}
+const fahrenheit = cToF(10);
+console.log(fahrenheit)
